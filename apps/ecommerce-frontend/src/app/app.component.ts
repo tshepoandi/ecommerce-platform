5b5662b8df6addb49ba4a34fde6bb2d69dff1709
tshepoandi <lovekilledtshepo@gmail.com>
2024-12-09 08:12:21 +0200

    footer

--- apps/ecommerce-frontend/src/app/app.component.ts
+++ apps/ecommerce-frontend/src/app/app.component.ts
@@ -7,9 +7,10 @@ import {
 } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awsome-icons';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
-  imports: [RouterModule, FontAwesomeModule, NavbarComponent],
+  imports: [RouterModule, FontAwesomeModule, FooterComponent, NavbarComponent],
   selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
