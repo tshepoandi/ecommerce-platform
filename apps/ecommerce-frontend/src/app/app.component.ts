d9001e148e426b91c9c4e25cf6a49491ca17d2b7
tshepoandi <lovekilledtshepo@gmail.com>
2024-12-09 06:44:00 +0200

    loading icons

--- apps/ecommerce-frontend/src/app/app.component.ts
+++ apps/ecommerce-frontend/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  FaConfig,
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awsome-icons';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecommerce-frontend';
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private FaConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.FaConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ apps/ecommerce-frontend/src/app/shared/font-awsome-icons.ts
@@ -0,0 +1,4 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [faUser];
